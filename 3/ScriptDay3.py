5476e47e2c77526e99ba1666b3e73e079d2b97dd
Erik Aho <erik.aho@argos.co.uk>
2019-12-04 11:20:37 +0000

    cleaned up day 3 a bit

--- 3/ScriptDay3.py
+++ 3/ScriptDay3.py
@@ -1,6 +1,3 @@
-with open('3/input.txt', 'r') as input_file:
-    raw_wires = input_file.readlines()
-
 def parse_instruction(instruction): #returns a vector
     (direction, magnitude) = (instruction[0], int(instruction[1:]))
     if direction == "R":
@@ -15,7 +12,7 @@ def parse_instruction(instruction): #returns a vector
         print('Error parsing instruction: ', instruction)
         return(0,0)
 
-def get_origin():
+def get_origin(): #Could be done with deep copy instead of function
     return (0,0)
 
 def get_manhattan(x,y):
@@ -33,11 +30,51 @@ def check_intersect(a,b):
         y_seg = a
     if(min(x_seg[0][0], x_seg[1][0]) <= y_seg[0][0] <= max(x_seg[0][0], x_seg[1][0])):
         if(min(y_seg[0][1], y_seg[1][1]) <= x_seg[0][1] <= max(y_seg[0][1], y_seg[1][1])):
-            return(y_seg[0][0], x_seg[0][1])
+            return(y_seg[0][0], x_seg[0][1]) #coordinates of intersection
         else:
             return False
     else:
         return False
+
+with open('3/input.txt', 'r') as input_file:
+    raw_wires = input_file.readlines()
+
+wires_instructions = [] #will contain the parsed input
+
+for wire in raw_wires:
+    instructions = wire.split(',')
+    new_wire = list(map(parse_instruction, instructions))
+    wires_instructions.append(new_wire)
+
+
+wire_maps = []
+for wire in wires_instructions:
+    start = get_origin()
+    wire_map = []
+    for segment_direction in wire:
+        end = add_segment(start,segment_direction)
+        wire_map.append((start,end))
+        start = end
+    wire_maps.append(wire_map)
+
+
+intersections = []
+for a in wire_maps[0]:
+    for b in wire_maps[1]:
+        res = check_intersect(a,b)
+        if res != False:
+            intersections.append(res)
+
+dist = []
+for intersect in intersections:
+    man_dist = get_manhattan(intersect[0], intersect[1])
+    if man_dist > 0:
+        dist.append(man_dist)
+print("Minimum manhattan distance to an intersection: ", min(dist))
+
+
+#Part 2
+
 
 def get_stepped_maps(wire_instructions):
     maps = []
@@ -53,53 +90,13 @@ def get_stepped_maps(wire_instructions):
         maps.append(wire_map)
     return maps
 
-wires_instructions = []
-
-for wire in raw_wires:
-    instructions = wire.split(',')
-    new_wire = list(map(parse_instruction, instructions))
-    wires_instructions.append(new_wire)
-
-wire_maps = []
-
-
-for wire in wires_instructions:
-    start = get_origin()
-    wire_map = []
-    for segment_direction in wire:
-        end = add_segment(start,segment_direction)
-        wire_map.append((start,end))
-        start = end
-    wire_maps.append(wire_map)
-
-
-intersections = []
-
-for a in wire_maps[0]:
-    for b in wire_maps[1]:
-        res = check_intersect(a,b)
-        if res != False:
-            intersections.append(res)
-
-dist = []
-
-for intersect in intersections:
-    man_dist = get_manhattan(intersect[0], intersect[1])
-    if man_dist > 0:
-        dist.append(man_dist)
-
-print("Minimum manhattan distance to an intersection: ", min(dist))
-
-
+def get_steps(point_1, point_2):
+    return(abs(point_1[0] - point_2[0]) + abs(point_1[1] - point_2[1]))
 
 
 stepped_maps = get_stepped_maps(wires_instructions)
 
-def get_steps(point_1, point_2):
-    return(abs(point_1[0] - point_2[0]) + abs(point_1[1] - point_2[1]))
-
 steps_to_intersections = []
-
 for a in stepped_maps[0]:
     for b in stepped_maps[1]:
         res = check_intersect(a,b)
@@ -111,4 +108,4 @@ for a in stepped_maps[0]:
             if tot_steps != 0:
                 steps_to_intersections.append(tot_steps)
 
-print('Minimum steps to intersections:' , min(steps_to_intersections)) # [1:] needed here to discount intersection where 
+print('Minimum steps to intersections:' , min(steps_to_intersections))
