24b48484964f444495e057d34c3f46aaddcfff28
Erik Aho <erik.aho@argos.co.uk>
2019-12-21 14:49:15 +0000

    Refactored Day 7

--- 7/Methods.py
+++ 7/Methods.py
@@ -1,69 +1,80 @@
 def opcode_1(array, modes, index):
-    params = get_params(array,modes,index)
-    array[(array[index+3])]= params[1] + params[0]
+    params = get_params(array, modes, index)
+    array[(array[index + 3])] = params[1] + params[0]
+
 
 def opcode_2(array, modes, index):
-    params = get_params(array,modes,index)
-    array[(array[index+3])] = params[1] * params[0]
+    params = get_params(array, modes, index)
+    array[(array[index + 3])] = params[1] * params[0]
 
-def opcode_3(array, modes, index, input_val):
-    address = array[index+1]
+
+def opcode_3(array, index, input_val):
+    address = array[index + 1]
     array[address] = input_val
 
-def opcode_4(array, modes, index):
-    address = int(array[index+1])
-    return(int(array[address]))
+
+def opcode_4(array, index):
+    address = int(array[index + 1])
+    return int(array[address])
+
 
 def opcode_5(array, modes, index):
-    params = get_params(array,modes,index)
+    params = get_params(array, modes, index)
     if params[0] != 0:
-        return(params[1])
-    else: 
-        return(index)
+        return params[1]
+    else:
+        return index
+
 
 def opcode_6(array, modes, index):
-    params = get_params(array, modes,index)
+    params = get_params(array, modes, index)
     if params[0] == 0:
-        return(params[1])
+        return params[1]
     else:
-        return(index)
+        return index
+
 
 def opcode_7(array, modes, index):
     params = get_params(array, modes, index)
     if params[0] < params[1]:
-        array[array[index+3]] = 1
+        array[array[index + 3]] = 1
     else:
-        array[array[index+3]] = 0
+        array[array[index + 3]] = 0
+
 
 def opcode_8(array, modes, index):
     params = get_params(array, modes, index)
     if params[0] == params[1]:
-        array[array[index+3]] = 1
+        array[array[index + 3]] = 1
     else:
-        array[array[index+3]] = 0
+        array[array[index + 3]] = 0
 
-def opcode_99(output):
+
+def opcode_99():
     pass
     # print('Code has completed. output is: ', output)
+
 
 def did_change(array, index, opcode):
     if ('0000' + str(array[index])) == opcode:
         return False
-    else: 
+    else:
         return True
+
 
 def get_params(array, modes, index):
     params = []
     modes = modes[::-1]
     for i in range(len(modes)):
-        v = int(array[index+i+1])
+        v = int(array[index + i + 1])
         if modes[i] == '0':
             params.append(array[v])
         elif modes[i] == '1':
             params.append(v)
     return params
 
-class opcode_comp:
+
+class OpcodeComp:
     def __init__(self, array):
         self.memory = list(array)
         self.input = 0
@@ -75,33 +86,31 @@ class opcode_comp:
 
     def set_phase(self, val: int):
         self.phase = val
-    
+
     def set_input(self, val: int):
         self.input = val
-    
+
     def read_input(self):
-        if self.isPhase == True:
+        if self.isPhase:
             self.isPhase = False
             return self.phase
-        else: 
+        else:
             return self.input
 
     def has_finished(self):
         return self.finished
-    
-    def set_output(self, val:int):
+
+    def set_output(self, val: int):
         self.output = val
 
     def get_output(self):
         return self.output
 
     def run_opcode(self):
-        finish_iter = False
-        while finish_iter == False: 
-            #init fn, get method code from opcode str
-            count = 0
+        while not self.finished:
+            # init fn, get method code from opcode str
             opcode = str(self.memory[self.index])
-            opcode = '0000' + opcode 
+            opcode = '0000' + opcode
             fn = opcode[-2:]
 
             if fn == '01':
@@ -112,10 +121,10 @@ class opcode_comp:
                 count = 4
             elif fn == '03':
                 input_val = self.read_input()
-                opcode_3(self.memory, '', self.index, input_val)
+                opcode_3(self.memory, self.index, input_val)
                 count = 2
             elif fn == '04':
-                self.output = opcode_4(self.memory, '', self.index)
+                self.output = opcode_4(self.memory, self.index)
                 # print('Output: ',self.output)
                 self.index += 2
                 break
@@ -127,26 +136,22 @@ class opcode_comp:
                 count = 3
             elif fn == '07':
                 opcode_7(self.memory, opcode[-5:-2], self.index)
-                count =  4
+                count = 4
             elif fn == '08':
                 opcode_8(self.memory, opcode[-5:-2], self.index)
                 count = 4
             elif fn == '99':
-                opcode_99(self.output)
+                opcode_99()
                 self.finished = True
-                finish_iter = True
                 break
             else:
                 print('Error')
                 break
 
-            #increment in case where instruction pointer has not changed
-            if did_change(self.memory, self.index, opcode) == True: 
+            # increment in case where instruction pointer has not changed
+            if did_change(self.memory, self.index, opcode):
                 continue
             else:
                 self.index += count
                 continue
-        return('Error')
-
-
-
+        return 'Error'
--- 7/ScriptDay7.py
+++ 7/ScriptDay7.py
@@ -1,59 +1,53 @@
 from itertools import permutations
-
-from Methods import opcode_comp
-
 from math import inf
 
-import copy
+from Methods import OpcodeComp
 
-def get_initial_state(): #could do with deep copy instead of fetching each time
+
+def get_initial_state():
     with open('7/input.txt', 'r') as input_file:
-        return(tuple(map(int, input_file.read().split(','))))
+        return tuple(map(int, input_file.read().split(',')))
 
-count_amps = 5
-max_out = 0
 
-initial_array = get_initial_state()
+def part_1():
+    max_out = 0
+    perms = permutations(range(count_amps))
 
-perms = permutations(range(count_amps))
+    for perm in perms:
+        output = 0
+        for phase in perm:
+            processor = OpcodeComp(initial_array)
+            processor.phase = phase
+            processor.input = output
+            processor.run_opcode()
+            output = processor.get_output()
+        if output > max_out:
+            max_out = output
+    return max_out
 
-for perm in perms:
 
-    output = 0
-    for phase in perm:
-        processor = opcode_comp(initial_array)
-        processor.phase = phase
-        processor.input = output
-        processor.run_opcode()
-        output = processor.get_output()
-    if output > max_out:
-        max_out = output
-        max_seq = perm
-
-print('Part 1:\nMaximum output: ', max_out)
-
-class setup_amps:
-    def __init__(self, initial_array, sequence):
-        self.memory = list(initial_array)
+class SetupAmps:
+    def __init__(self, initial_state, sequence):
+        self.memory = list(initial_state)
         self.sequence = sequence
         self.count_amps = 5
         self.final_amp = self.count_amps - 1
-        self.amps = [opcode_comp(self.memory) for _ in range(self.count_amps)]
+        self.amps = [OpcodeComp(self.memory) for _ in range(self.count_amps)]
         self.set_sequence(sequence)
 
     def set_sequence(self, sequence):
         for i in range(self.count_amps):
             self.amps[i].set_phase(sequence[i])
-        
+
     def run(self):
         a = self.amps[0]
         e = self.amps[-1]
-        e.set_output(0) #sets input for first run on A to be 0
+        e.set_output(0)  # sets input for first run on A to be 0
         index = 0
-        
+
         while True:
             current_amp = self.amps[index]
-            last_amp = self.amps[index-1]
+            last_amp = self.amps[index - 1]
             current_amp.set_input(last_amp.get_output())
             current_amp.run_opcode()
 
@@ -63,19 +57,27 @@ class setup_amps:
                 index += 1
                 if index > self.final_amp:
                     index = 0
-        
+
     def get_output(self):
         return self.amps[-1].get_output()
 
-perms = permutations(range(5,10))
-max_out = -inf
 
-for perm in perms:
-    setup = setup_amps(initial_array, perm)
-    setup.run()
-    output = setup.get_output()
-    if output > max_out:
-        max_out = output
+def part_2():
+    perms = permutations(range(5, 10))
+    max_out = -inf
 
-print('\nPart 2:\nMaximum output:', max_out)
+    for perm in perms:
+        setup = SetupAmps(initial_array, perm)
+        setup.run()
+        output = setup.get_output()
+        if output > max_out:
+            max_out = output
+    return max_out
 
+
+if __name__ == "__main__":
+    count_amps = 5
+    initial_array = get_initial_state()
+
+    print('Part 1:\nMaximum output: ', part_1())
+    print('Part 1:\nMaximum output: ', part_2())
