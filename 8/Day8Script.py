433d0bec7631acfb2f19e9aa766c1a2cc7ed736f
Erik Aho <erik.aho@argos.co.uk>
2019-12-21 14:59:21 +0000

    Refactored Day 8

--- 8/Day8Script.py
+++ 8/Day8Script.py
@@ -1,37 +1,39 @@
 from math import inf
 
+
 def get_input():
     with open('8/input.txt', 'r') as input_file:
-        return (input_file.read())
+        return input_file.read()
 
-input = get_input()
-len_x = 25
-len_y = 6
-layer_size = len_x * len_y
 
-layers = [input[i:i+layer_size] for i in range(0,len(input),layer_size)]
+def get_layers(inp):
+    return [image_input[i:i + layer_size] for i in range(0, len(inp), layer_size)]
 
-result = 0
-min_0s = inf
 
-for layer in layers:
-    count0 = layer.count('0')
-    if count0 < min_0s:
-        min_0s = count0
+def part_1():
+    result = 0
+    min_0s = inf
 
-print('2s*1s = ', result)
+    for layer in layers:
+        count0 = layer.count('0')
+        if count0 < min_0s:
+            min_0s = count0
+
+    return result
+
 
 def get_pixel(pixels):
     for pixel in pixels:
         if pixel == '0':
-            return '0'
-        elif pixel =='1':
-            return '1'
-        elif pixel =='2':
+            return ' '
+        elif pixel == '1':
+            return '*'
+        elif pixel == '2':
             continue
         else:
             print('ERROR')
             break
+
 
 def to_image(image):
     img_str = ''
@@ -41,14 +43,26 @@ def to_image(image):
         img_str += '\n'
     return img_str
 
-image = []
 
-for y in range(len_y):
-    row = []
-    for x in range(len_x):
-        i = (len_x * y) + x
-        pixels = [layer[i] for layer in layers]
-        row.append(get_pixel(pixels))
-    image.append(row)
+def get_image():
+    image = []
 
-print(to_image(image))
+    for y in range(len_y):
+        row = []
+        for x in range(len_x):
+            i = (len_x * y) + x
+            pixels = [layer[i] for layer in layers]
+            row.append(get_pixel(pixels))
+        image.append(row)
+    return to_image(image)
+
+
+if __name__ == '__main__':
+    image_input = get_input()
+    len_x = 25
+    len_y = 6
+    layer_size = len_x * len_y
+    layers = get_layers(image_input)
+
+    print('2s*1s = ', part_1())
+    print("resulting image: \n" + get_image())
