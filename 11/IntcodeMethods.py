2f8c3218f963d5f57ace685474b55f94fef15dd1
Erik Aho <erik.aho@argos.co.uk>
2019-12-21 18:46:53 +0000

    Refactor Day 11

--- 11/IntcodeMethods.py
+++ 11/IntcodeMethods.py
@@ -1,8 +1,9 @@
-#unchanged from day 9
+# unchanged from day 9
 
 from collections import defaultdict
 
-class opcode_comp:
+
+class OpcodeComp:
     def __init__(self, array):
         self.memory = defaultdict(lambda: 0, enumerate(array))
         self.inputs = []
@@ -20,27 +21,27 @@ class opcode_comp:
     def add_input(self, *inputs: int):
         for i in inputs:
             self.inputs.append(i)
-    
+
     def read_input(self) -> int:
-        if self.isPhase == True:
+        if self.isPhase:
             self.isPhase = False
             return self.phase
         else:
-            return (self.inputs.pop(0))
-        
+            return self.inputs.pop(0)
+
     def has_finished(self):
         return self.finished
-    
-    def set_output(self, val:int):
+
+    def set_output(self, val: int):
         self.output = val
-    
+
     def get_output(self):
         return self.output
-    
+
     def change_base(self, val: int):
         self.base += val
 
-    def interpret_params(self,modes:list):
+    def interpret_params(self, modes: list):
         params = []
         for i in range(len(modes)):
             v = int(self.memory[self.index + i + 1])
@@ -51,8 +52,8 @@ class opcode_comp:
             elif modes[i] == '2':
                 params.append(int(self.memory[v + self.base]))
         return params
-    
-    def literal_params(self, modes:list):
+
+    def literal_params(self, modes: list):
         params = []
         for i in range(len(modes)):
             v = int(self.memory[self.index + i + 1])
@@ -63,13 +64,12 @@ class opcode_comp:
             elif modes[i] == '2' or 2:
                 params.append(v + self.base)
         return params
-    
+
     def run(self):
-        while self.finished == False:
+        while not self.finished:
             opcode = '0000' + str(self.memory[self.index])
             fn = opcode[-2:]
             modes = list(opcode[-3:-6:-1])
-            nums = [self.memory[i] for i in range(self.index, self.index +4)]
             # print('Fn: {}, Modes: {}, Index: {}, Numbers: {}'.format(fn, modes, self.index,nums))
             if fn == '01':
                 params = self.interpret_params(modes)
@@ -130,7 +130,3 @@ class opcode_comp:
             else:
                 print('Error')
                 break
-            
-            
-
-    
--- 11/ScriptDay11.py
+++ 11/ScriptDay11.py
@@ -1,27 +1,30 @@
-from IntcodeMethods import opcode_comp
+from IntcodeMethods import OpcodeComp
 from collections import defaultdict
 import operator
 
-with open('11/input.txt','r') as input_file:
+with open('11/input.txt', 'r') as input_file:
     initial_state = input_file.read().split(',')
 
-def rotate(dir,val): #0 -> turn left(counterclockwise), 1 -> turn right(clockwise)
-    if val == 0:
-        return(-dir[1], dir[0])
-    elif val == 1:
-        return(dir[1], -dir[0])
 
-class paint_robot:
+def rotate(cur_dir, val):  # 0 -> turn left(counterclockwise), 1 -> turn right(clockwise)
+    """returns rotated direction (left or right) based on val being 0 or 1"""
+    if val == 0:
+        return -cur_dir[1], cur_dir[0]
+    elif val == 1:
+        return cur_dir[1], -cur_dir[0]
+
+
+class PaintRobot:
     def __init__(self):
-        self.computer = opcode_comp(initial_state)
-        self.position = (0,0) #position is x,y cartesian coord
-        self.direction = (0,1) # dir is unit cartesian vector
-        self.been_painted = set({}) #needed because of how defaultdict handles looking at dict as creating a val
+        self.computer = OpcodeComp(initial_state)
+        self.position = (0, 0)  # position is x,y cartesian coord
+        self.direction = (0, 1)  # dir is unit cartesian vector
+        self.been_painted = set({})  # needed because of how defaultdict handles looking at dict as creating a val
         self.map = defaultdict(int)
 
     def do_rot(self, val):
         self.direction = rotate(self.direction, val)
-    
+
     def do_move(self):
         new_pos = tuple(map(operator.add, self.position, self.direction))
         self.position = new_pos
@@ -29,47 +32,50 @@ class paint_robot:
     def check_panel(self):
         return self.map[self.position]
 
-    def paint_panel(self, colour): #add position to dict and paint it
+    def paint_panel(self, colour):  # add position to dict and paint it
         self.map[self.position] = colour
 
     def run_paint(self):
         while True:
-            #check for input, then paint
+            # check for input, then paint
             self.computer.add_input(self.check_panel())
             self.computer.run()
             if self.computer.finished is True:
                 break
             paint_colour = self.computer.get_output()
             self.paint_panel(paint_colour)
-            if self.position not in self.been_painted: 
+            if self.position not in self.been_painted:
                 self.been_painted.add(self.position)
 
-            #rotate and move
+            # rotate and move
             self.computer.run()
             direction = self.computer.get_output()
             self.do_rot(direction)
             self.do_move()
 
-#Part 1
-robot = paint_robot() 
-robot.run_paint()
-print('\nNumber of unique panels painted: ', len(robot.been_painted))
 
-#Part 2
-robot2 = paint_robot()
-robot2.map[(0,0)] = 1
-robot2.run_paint()
+if __name__ == '__main__':
 
-xr, yr = zip(*(robot2.map.keys()))
-min_x, max_x, min_y, max_y = min(xr), max(xr), min(yr), max(yr)
+    # Part 1
+    robot = PaintRobot()
+    robot.run_paint()
+    print('\nNumber of unique panels painted: ', len(robot.been_painted))
 
-output = '\n'
-for j in range(max_y, min_y-1, -1):
-    for i in range(min_x, max_x, 1):
-        if robot2.map[(i,j)] == 1:
-            output += ('█')
-        else:
-            output += ('░')
-    output += ('\n')
+    # Part 2
+    robot2 = PaintRobot()
+    robot2.map[(0, 0)] = 1
+    robot2.run_paint()
 
-print('\nCode for part 2: \n',output)
+    xr, yr = zip(*(robot2.map.keys()))
+    min_x, max_x, min_y, max_y = min(xr), max(xr), min(yr), max(yr)
+
+    output = '\n'
+    for j in range(max_y, min_y - 1, -1):
+        for i in range(min_x, max_x, 1):
+            if robot2.map[(i, j)] == 1:
+                output += '█'
+            else:
+                output += ' '
+        output += '\n'
+
+    print('\nCode for part 2: \n', output)
